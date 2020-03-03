0a86010175bdc38c4c8f9d4be0de96b3565013c8
StarwarsShooter <61284683+StarwarsShooter@users.noreply.github.com>
2020-03-03 09:48:55 +0100

    Add project files.

---
+++ circustrein/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace circustrein
+{
+    class Program
+    {
+        string eter;
+        int formaat;
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Is het een vleeseter of planteneter?");
+            eter = Console.ReadLine();
+            Console.WriteLine("Is het een klein dier (1) een middelgroot dier (3) of een groot dier (5)");
+            string grootte = Console.ReadLine();
+            formaat = Convert.ToInt32(grootte);
+        }
+        
+    }
+}
