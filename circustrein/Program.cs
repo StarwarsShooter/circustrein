616fb7cc58eff83b4bd38c43b33e354ae9947b63
StarwarsShooter <61284683+StarwarsShooter@users.noreply.github.com>
2020-03-09 13:43:29 +0100

    first edit

--- circustrein/Program.cs
+++ circustrein/Program.cs
@@ -13,7 +13,8 @@ namespace circustrein
             Console.WriteLine("Is het een klein dier (1) een middelgroot dier (3) of een groot dier (5)");
             string grootte = Console.ReadLine();
             formaat = Convert.ToInt32(grootte);
-        }
-        
+
+            animal animal = new animal(eter, formaat);
+        }        
     }
 }
---
+++ circustrein/animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace circustrein
+{
+    class animal
+    {
+        private string food;
+        private int size;
+
+        public string AnimalFood { get { return food; } }
+        public int AnimalSize { get { return size; } }
+        
+        public animal(string food, int size)
+        {
+            this.food = food;
+            this.size = size;
+        }
+    }
+}
